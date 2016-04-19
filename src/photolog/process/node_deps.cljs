053dd39f0d45de0f2f75c20627828e17fa11e8ac
Rob Burns <rburns@paiges.net>
2016-04-19 20:39:01 +0700

    remove unused node dependencies

--- src/photolog/process/node_deps.cljs
+++ src/photolog/process/node_deps.cljs
@@ -1,12 +1,7 @@
 (ns photolog.process.node-deps
   (:require [cljs.nodejs :as node]))
 
-(def readdir-sync (.-readdirSync (node/require "fs")))
 (def write-file-sync (.-writeFileSync (node/require "fs")))
-
 (def resolve-path (.-resolve (node/require "path")))
-(def path-extension (.-extname (node/require "path")))
-
 (def exec-sync (.-execSync (node/require "child_process")))
-
 (def sharp (node/require "sharp"))
