190cf57b2a8c793d0c5dbd0315cc360a5fcffa89
Rob Burns <rburns@paiges.net>
2016-10-21 00:18:06 +0700

    whitespace

--- test/photolog/process/async_test.cljs
+++ test/photolog/process/async_test.cljs
@@ -4,35 +4,35 @@
             [photolog.process.async :as a])
   (:require-macros [cljs.core.async.macros :refer [go]]))
 
+;; things it doesn't do
+;;
+;; handle async functions with callback which doesn't follow the error, result convention
+;; handle async function which take 0 arguments, apart from the callback
+
 (deftest async-function-returns-a-channel
   (async done
-   (let [echo-cb   (fn [a cb] (cb nil a))
-         echo-chan (a/->single-value-chan echo-cb)]
-     (go (is (= 1 (<! (echo-chan 1))))
-         (done)))))
+         (let [echo-cb   (fn [a cb] (cb nil a))
+               echo-chan (a/->single-value-chan echo-cb)]
+           (go (is (= 1 (<! (echo-chan 1))))
+               (done)))))
 
 (deftest variable-arity-async-function
   (async done
-   (let [echo-cb   (fn [a b c cb] (cb nil [a b c]))
-         echo-chan (a/->single-value-chan echo-cb)]
-     (go (is (= [1 2 3] (<! (echo-chan 1 2 3))))
-         (done)))))
+         (let [echo-cb   (fn [a b c cb] (cb nil [a b c]))
+               echo-chan (a/->single-value-chan echo-cb)]
+           (go (is (= [1 2 3] (<! (echo-chan 1 2 3))))
+               (done)))))
 
 (deftest propagates-errors
   (async done
-   (let [error-cb   (fn [a cb] (cb "error" nil))
-         error-chan (a/->single-value-chan error-cb)]
-     (go (is (= {:error "error"} (<! (error-chan 1))))
-         (done)))))
+         (let [error-cb   (fn [a cb] (cb "error" nil))
+               error-chan (a/->single-value-chan error-cb)]
+           (go (is (= {:error "error"} (<! (error-chan 1))))
+               (done)))))
 
 (deftest propagates-exceptions
   (async done
-   (let [error-cb   (fn [a cb] (throw "error"))
-         error-chan (a/->single-value-chan error-cb)]
-     (go (is (= {:error "error"} (<! (error-chan 1))))
-         (done)))))
-
-;; things it doesn't do
-;;
-;; handle async functions with callback which don't follow the error, result convention
-;; handle async function which take 0 arguments, apart from the callback
+         (let [error-cb   (fn [a cb] (throw "error"))
+               error-chan (a/->single-value-chan error-cb)]
+           (go (is (= {:error "error"} (<! (error-chan 1))))
+               (done)))))
