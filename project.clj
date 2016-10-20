6a85d8c8b3a217f9437fecc0eeb31373a590af12
Rob Burns <rburns@paiges.net>
2016-10-20 22:23:36 +0700

    update dependencies

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject photolog "0.1.0-SNAPSHOT"
   :description "display photos"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.229"]
-                 [com.cognitect/transit-cljs "0.8.237"]])
+                 [org.clojure/clojurescript "1.9.293"]
+                 [org.clojure/core.async "0.2.395"]
+                 [com.cognitect/transit-cljs "0.8.239"]])
