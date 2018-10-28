0917ffed4226b095a38e57a96df973110d851110
Rob Burns <rburns@paiges.net>
2018-10-28 14:03:28 +0700

    upgrade clojure dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject photolog "0.1.0-SNAPSHOT"
   :description "display photos"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.671"]
-                 [org.clojure/core.async "0.3.443"]
-                 [com.cognitect/transit-cljs "0.8.239"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojurescript "1.10.238"]
+                 [org.clojure/core.async "0.4.474"]
+                 [com.cognitect/transit-cljs "0.8.256"]])
